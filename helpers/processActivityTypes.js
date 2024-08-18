9e00bd24751cd4ac7a0a330d22710b77619e16ae
Martin Cox <martincox99@me.com>
2024-08-18 08:46:52 -0500

    refactor: Improve activity type processing logic and status handling. Change status to "started" and "not-started"

--- helpers/processActivityTypes.js
+++ helpers/processActivityTypes.js
@@ -16,10 +16,12 @@ export const processActivityTypes = async (activityTypes, category) => {
       let activityLabel = activityType.name;
       // Activity status
       let activityStatus = "none";
-      // Check if the activity type is a "start" activity
-      const isStartActivity = lastActivity && lastActivity.status === "start";
-      // Check if the activity type is an "end" activity
-      const isEndActivity = lastActivity && lastActivity.status === "end";
+      // Check if the activity is "started" or not
+      const isStartedActivity =
+        lastActivity && lastActivity.status === "started";
+      // Check if the activity is "not-started" or not
+      const isEndedActivity =
+        lastActivity && lastActivity.status === "not-started";
       // Check if the activity type is an "uncategorized" activity
       const isUncategorized = category === "Uncategorized";
 
@@ -27,13 +29,21 @@ export const processActivityTypes = async (activityTypes, category) => {
       if (!lastActivity) {
         // Check if the activity type is a toggle activity
         if (!activityType.toggle) {
+          // Since it's NOT a toggle activity
+          // - Set the time elapsed to "N/A"
+          // - Set the activity label to the activity type name
+          // - Set the activity status to "none"
           timeElapsed = "N/A";
           activityLabel = activityType.name;
+          activityStatus = "none";
         } else if (activityType.toggle) {
           // If there is no logged activity AND the activity type is a toggle activity
+          // - Set the time elapsed to "N/A"
+          // - Set the activity label to the start activity label
+          // - Set the activity status to "not-started"
           timeElapsed = "N/A";
           activityLabel = activityType.start_label;
-          activityStatus = "start";
+          activityStatus = "not-started";
         }
       } else if (lastActivity) {
         // There is a logged activity of this type
@@ -41,19 +51,48 @@ export const processActivityTypes = async (activityTypes, category) => {
         timeElapsed = getTimeDifference(lastActivity.timestamp);
         // Check if the activity type is a toggle activity
         if (!activityType.toggle) {
+          // If the activity type is NOT a toggle activity AND there is a logged activity
+          // - Set the activity label to the activity type name
+          // - Set the activity status to "none"
           activityLabel = activityType.name;
           activityStatus = "none";
         } else if (activityType.toggle) {
           // If the activity type is a toggle activity
-          // Check if the last activity was a start activity
-          if (isStartActivity) {
-            // Set the activity label to the end activity label
-            activityLabel = activityType.end_label;
-            activityStatus = "start";
-          } else if (isEndActivity) {
-            // If the last activity was an end activity, set the activity label to the start activity label
+          // Check if the last activity was STARTED
+          if (isStartedActivity) {
+            // - Set the activity label to the end activity label
+            // - Set the activity status to "started"
+            // Add the game, movie, or TV show to the label, if any
+            switch (activityType.name.toLowerCase()) {
+              case "gaming":
+                const game = lastActivity.description.split("Game: ")[1];
+                activityLabel = activityType.end_label.replace(/gaming/i, game);
+                break;
+              case "watching tv":
+                const tvShow = lastActivity.description.split("Show: ")[1];
+                activityLabel = activityType.end_label.replace(
+                  /watching tv/i,
+                  tvShow
+                );
+                break;
+              case "watching movie":
+                const movie = lastActivity.description.split("Movie: ")[1];
+                activityLabel = activityType.end_label.replace(
+                  /watching movie/i,
+                  movie
+                );
+                break;
+              default:
+                activityLabel = activityType.end_label;
+                break;
+            }
+            activityStatus = "started";
+          } else if (isEndedActivity) {
+            // If the last activity was NOT started
+            // - Set the activity label to the start activity label
+            // - Set the activity status to "not-started"
             activityLabel = activityType.start_label;
-            activityStatus = "end";
+            activityStatus = "not-started";
           }
         }
       } // End of if (lastActivity)
@@ -66,21 +105,26 @@ export const processActivityTypes = async (activityTypes, category) => {
       });
 
       // Process it if it's a started activity
-      if (isStartActivity) {
+      if (isStartedActivity) {
+        // Add the started activity to the startedMenuItems array
         startedMenuItems.push(`${activityLabel} (${timeElapsed})`);
+        // Return null for the activity name and time elapsed to skip it
         return { name: null, timeElapsed: null };
       }
 
       // Check if the activity type is uncategorized
       if (isUncategorized) {
+        // Add the uncategorized activity to the uncategorizedItems array
         uncategorizedItems.push({
           name: activityLabel,
           timestamp: lastActivity ? lastActivity.timestamp : 0,
           timeElapsed,
         });
+        // Return null for the activity name and time elapsed to skip it
         return { name: null, timeElapsed: null };
       }
 
+      // Return the activity type name and time elapsed
       return { name: activityLabel, timeElapsed };
     })
   ); // End of Promise.all
